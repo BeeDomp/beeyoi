5f677899514da9221d562ad692bf5a2387bde4b5
iamuput <yogaaasp73@gmail.com>
2024-03-07 20:46:24 +0700

    Update autoleave.py

--- Auput/plugins/misc/autoleave.py
+++ Auput/plugins/misc/autoleave.py
@@ -1,19 +1,10 @@
-#
-# Copyright (C) 2021-2022 by TeamYukki@Github, < https://github.com/TeamYukki >.
-#
-# This file is part of < https://github.com/TeamYukki/YukkiMusicBot > project,
-# and is released under the "GNU v3.0 License Agreement".
-# Please see < https://github.com/TeamYukki/YukkiMusicBot/blob/master/LICENSE >
-#
-# All rights reserved.
-
 import asyncio
 from datetime import datetime
 
 import config
-from YukkiMusic import app
-from YukkiMusic.core.call import Yukki, autoend
-from YukkiMusic.utils.database import (get_client, is_active_chat,
+from Auput import app
+from Auput.core.call import Auput, autoend
+from Auput.utils.database import (get_client, is_active_chat,
                                        is_autoend)
 
 
@@ -22,7 +13,7 @@ async def auto_leave():
         while not await asyncio.sleep(
             config.AUTO_LEAVE_ASSISTANT_TIME
         ):
-            from YukkiMusic.core.userbot import assistants
+            from Auput.core.userbot import assistants
 
             for num in assistants:
                 client = await get_client(num)
@@ -73,7 +64,7 @@ async def auto_end():
                     continue
                 autoend[chat_id] = {}
                 try:
-                    await Yukki.stop_stream(chat_id)
+                    await Auput.stop_stream(chat_id)
                 except:
                     continue
                 try:
