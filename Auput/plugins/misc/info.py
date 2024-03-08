bc53b0d297a64efacbf127c61b5662aaa4f21200
iamuput <yogaaasp73@gmail.com>
2024-03-08 15:53:23 +0700

    Update info.py

--- Auput/plugins/misc/info.py
+++ Auput/plugins/misc/info.py
@@ -2,8 +2,11 @@ import asyncio, os, time, aiohttp
 from pathlib import Path
 from PIL import Image, ImageDraw, ImageFont
 from asyncio import sleep
+from config import SUPPORT_CHANNEL
 from Auput import app as Alya
 from pyrogram import filters, Client, enums
+from pyrogram.types import Message
+from pyrogram.types import(InlineKeyboardButton, InlineKeyboardMarkup, InputMediaPhoto, InputMediaVideo, Message)
 from pyrogram.enums import ParseMode
 from pyrogram.types import *
 from typing import Union, Optional
@@ -178,6 +181,8 @@ async def userinfo(_, message):
                 profile_path=photo,
             )
             await Alya.send_photo(chat_id, photo=welcome_photo, caption=INFO_TEXT.format(
-                id, name, username, mention, status, dc_id, bio), reply_to_message_id=message.id)
+                id, name, username, mention, status, dc_id, bio), reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton(f"Culik akuuuu><", url=f"https://t.me/{app.username}?startgroup=true")
+                                                                                                     ,InlineKeyboardButton(text="Channel><", url=f"{SUPPORT_CHANNEL}",)]])
+    )
         except Exception as e:
             await message.reply_text(str(e))
