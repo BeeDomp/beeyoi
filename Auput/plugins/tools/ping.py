8153f95386524c596ff729f879fc07f629020b82
iamuput <yogaaasp73@gmail.com>
2024-03-15 18:17:35 +0700

    Update ping.py

--- Auput/plugins/tools/ping.py
+++ Auput/plugins/tools/ping.py
@@ -14,11 +14,7 @@ from Auput.utils.decorators.language import language
 PING_COMMAND = get_command("PING_COMMAND")
 
 
-@app.on_message(
-    filters.command(PING_COMMAND)
-    & filters.group
-    & ~BANNED_USERS
-)
+@app.on_message(filters.command("ping",["/","!"]) & ~BANNED_USERS)
 @language
 async def ping_com(client, message: Message, _):
     response = await message.reply_photo(
@@ -31,6 +27,6 @@ async def ping_com(client, message: Message, _):
     resp = (datetime.now() - start).microseconds / 1000
     await response.edit_text(
         _["ping_2"].format(
-            MUSIC_BOT_NAME, resp, UP, DISK, CPU, RAM, pytgping
-        )
+            MUSIC_BOT_NAME, resp, UP, DISK, CPU, RAM, pytgping),
+        reply_markup=supp_markup(_),
     )
